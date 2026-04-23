1ff5aecb61c080e6daeb10a7806d0fe08f4c825a
Roman <zelenkovr32@gmail.com>
2026-04-23 15:13:12 +0500

    init

--- Reactor.cs
+++ Reactor.cs
@@ -8,5 +8,16 @@ namespace Reactor
 {
     internal class Reactor
     {
+        public int Temperature {  get; set; }
+        public int Vater {  get; set; }
+        public int Radiation { get; set; }
+
+        public Reactor(int temperature,int vater,int radiation) 
+        {
+            this.Temperature = temperature;
+            this.Vater = vater;
+            this.Radiation = radiation;
+        }
+
     }
 }
